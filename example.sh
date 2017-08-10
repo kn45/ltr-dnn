b441c22ae33d71f5e1338fe6dbdf4e971e3043ff
kn45 <381404825@qq.com>
2017-08-10 15:55:29 +0800

    use a psuedo data for evaluation

--- example.sh
+++ example.sh
@@ -2,9 +2,8 @@
 
 python train.py \
 --train_bs=128 \
---max_epoch=20 \
+--max_epoch=50 \
 --eps=0.5 \
---eval_steps=1
-
-#--max_iter=100 \
+--eval_steps=1 \
+--max_iter=100000
 
--- train.py
+++ train.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 from ltrdnn import LTRDNN
 
 
-
 flags = tf.flags
 FLAGS = flags.FLAGS
 # model related:
@@ -19,7 +18,7 @@ flags.DEFINE_string('combiner', 'sum', 'how to combine words in a sentence')
 # training related:
 flags.DEFINE_integer('train_bs', 128, 'train batch size')
 flags.DEFINE_integer('max_epoch', 1, 'max epoch')
-flags.DEFINE_integer('max_iter', 100, 'max iteration')
+flags.DEFINE_integer('max_iter', 1000, 'max iteration')
 flags.DEFINE_float('eps', 1.0, 'zero-loss threshold epsilon in hinge loss')
 flags.DEFINE_integer('eval_steps', 20, 'every how many steps to evaluate')
 
@@ -27,6 +26,7 @@ flags.DEFINE_integer('eval_steps', 20, 'every how many steps to evaluate')
 def ramdon_choose(l):
     rand_obj = random.sample(l, 1)[0]
     return rand_obj
+
 
 def inp_fn(data):
     q_indices = []
@@ -64,40 +64,54 @@ def inp_fn(data):
            (pt_indices, pt_values, [batch_size, seq_len]), \
            (nt_indices, nt_values, [batch_size, seq_len])
 
+
 def eval_fn(inst):
-    q_indices = []
-    pt_indices = []
-    nt_indices = []
-    q_values = []
-    pt_values = []
-    nt_values = []
-    batch_size = 1
+    get_len = lambda lst: max([len(x) for x in lst])
     flds = inst.split('\t')
-    query = map(int, flds[0].split(' '))
-    pos_title = map(int, flds[1].split(' '))
-    neg_title = map(int, flds[2].split(' '))
-    seq_len = max(len(query), len(pos_title), len(neg_title))
-    i = 0
-    for j, word_id in enumerate(query):
-        q_indices.append([i, j])
-        q_values.append(word_id)
-    for j, word_id in enumerate(pos_title):
-        pt_indices.append([i, j])
-        pt_values.append(word_id)
-    for j, word_id in enumerate(neg_title):
-        nt_indices.append([i, j])
-        nt_values.append(word_id)
-    return [(q_indices, q_values, [batch_size, seq_len])], \
-           [(pt_indices, pt_values, [batch_size, seq_len])], \
-           [(nt_indices, nt_values, [batch_size, seq_len])]
+    queries = flds[0:1]
+    pos_title_num = int(flds[1])
+    pos_titles = flds[2:2+pos_title_num]
+    neg_title_num = int(flds[2+pos_title_num])
+    neg_titles = flds[2+pos_title_num+1:]
+
+    queries = [map(int, x.split(' ')) for x in queries]
+    pos_titles = [map(int, x.split(' ')) for x in pos_titles]
+    neg_titles = [map(int, x.split(' ')) for x in neg_titles]
+    seq_len = max(get_len(queries), get_len(pos_titles), get_len(neg_titles))
+
+    qry_out = []
+    for query in queries:
+        qry_idx = []
+        qry_val = []
+        for j, word_id in enumerate(query):
+            qry_idx.append([0, j])
+            qry_val.append(word_id)
+        qry_out.append((qry_idx, qry_val, [1, seq_len]))
+    pos_out = []
+    for pos_title in pos_titles:
+        pos_idx = []
+        pos_val = []
+        for j, word_id in enumerate(pos_title):
+            pos_idx.append([0, j])
+            pos_val.append(word_id)
+        pos_out.append((pos_idx, pos_val, [1, seq_len]))
+    neg_out = []
+    for neg_title in neg_titles:
+        neg_idx = []
+        neg_val = []
+        for j, word_id in enumerate(neg_title):
+            neg_idx.append([0, j])
+            neg_val.append(word_id)
+        neg_out.append((neg_idx, neg_val, [1, seq_len]))
+    return qry_out, pos_out, neg_out
 
 
-train_file = './3.train.negtive_sampled.ids'
-test_file = './3.train.negtive_sampled.ids.pair'
-#train_file = './data_train_example.tsv'
-#test_file = './data_train_example.tsv'
-#test_file = './data/3.test.negtive_sampled.ids'
-train_freader = dataproc.BatchReader(train_file, max_epoch=FLAGS.max_iter)
+# train_file = './3.train.negtive_sampled.ids'
+# test_file = './3.train.negtive_sampled.ids.pair'
+# test_file = './data/3.test.negtive_sampled.ids'
+train_file = './data_train_example.tsv'
+test_file = './data_test_example.tsv'
+train_freader = dataproc.BatchReader(train_file, max_epoch=FLAGS.max_epoch)
 with open(test_file) as f:
     test_data = [x.rstrip('\n') for x in f.readlines()]
 test_q, test_pt, test_nt = inp_fn(test_data)
@@ -123,13 +137,6 @@ for niter in xrange(FLAGS.max_iter):
     train_eval = mdl.eval_step(sess, train_q, train_pt, train_nt, metrics)
     test_eval = mdl.eval_step(sess, test_q, test_pt, test_nt, metrics) \
         if niter % FLAGS.eval_steps == 0 else 'SKIP'
-    #pred_diff = mdl.predict_diff(sess, train_q, train_pt, train_nt)
-    #pred_qp = mdl.predict_sim_qt(sess, train_q, train_pt)
-    #pred_qn = mdl.predict_sim_qt(sess, train_q, train_nt)
-    #pred_qq = mdl.predict_sim_qq(sess, train_q, train_q)
-    #print niter, 'train_loss:', train_eval, 'test_loss:', test_eval, \
-    #    'diff 0/1:', pred_diff, 'sim_qp:', pred_qp, 'sim_qn', pred_qn, \
-    #    'sim_qq:', pred_qq
     print niter, 'train_loss:', train_eval, 'test_loss:', test_eval
 
 feval = open(test_file)
