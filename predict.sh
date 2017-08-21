17bc85c485b78c0b25717b11f09b1ad4de76af29
dream2009gd <dream2009gd@163.com>
2017-08-21 14:46:15 +0800

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#cat ../data/3.test.negtive_sampled.ids | python predict.py
-cat ../data/input.dat1.ids | python predict.py
+cat pred.dat.ids | python predict.py
+#cat ../data/input.dat1.ids | python predict.py
 
